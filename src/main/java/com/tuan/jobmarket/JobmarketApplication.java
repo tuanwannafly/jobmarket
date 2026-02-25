0ce1c13bf3299be5420338a2af5dcd5789587c98
testbest <tunawillgetit1030@gmail.com>
2026-02-25 16:54:39 +0700

    adding base project

---
+++ src/main/java/com/tuan/jobmarket/JobmarketApplication.java
@@ -0,0 +1,13 @@
+package com.tuan.jobmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuan/jobmarket/JobmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuan.jobmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
