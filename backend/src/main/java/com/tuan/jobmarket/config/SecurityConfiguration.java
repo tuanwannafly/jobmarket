5c99409871ed5dfe6627fbfe1fd5aa4af7ae2129
testbest <tunawillgetit1030@gmail.com>
2026-02-28 01:02:08 +0700

    update security confi for logout user

--- backend/src/main/java/com/tuan/jobmarket/config/SecurityConfiguration.java
+++ backend/src/main/java/com/tuan/jobmarket/config/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration {
             .cors(Customizer.withDefaults())
             .authorizeHttpRequests(
                 authz -> authz
-                        .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
+                        .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/logout").permitAll()
                         .anyRequest().authenticated())
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                 .authenticationEntryPoint(customAuthenticationEntryPoint))
