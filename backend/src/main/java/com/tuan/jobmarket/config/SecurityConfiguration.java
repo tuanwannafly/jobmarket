8da5159bdeb3981a138a9155f76fe0118da6c50d
testbest <tunawillgetit1030@gmail.com>
2026-02-28 01:07:15 +0700

    update done

--- backend/src/main/java/com/tuan/jobmarket/config/SecurityConfiguration.java
+++ backend/src/main/java/com/tuan/jobmarket/config/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration {
             .cors(Customizer.withDefaults())
             .authorizeHttpRequests(
                 authz -> authz
-                        .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/logout").permitAll()
+                        .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
                         .anyRequest().authenticated())
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                 .authenticationEntryPoint(customAuthenticationEntryPoint))
