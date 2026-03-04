d2034c92074094461d4bb5dfb30179ad0fd9221b
testbest <tunawillgetit1030@gmail.com>
2026-03-04 13:13:51 +0700

    update upload cv

---
+++ backend/src/main/java/com/tuan/jobmarket/config/StaticResourcesWebConfiguration.java
@@ -0,0 +1,21 @@
+package com.tuan.jobmarket.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourcesWebConfiguration
+        implements WebMvcConfigurer {
+
+    @Value("${tuanjobmarket.upload-file.base-path}")
+    private String basePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/storage/**")
+                .addResourceLocations(basePath);
+    }
+    
+}
