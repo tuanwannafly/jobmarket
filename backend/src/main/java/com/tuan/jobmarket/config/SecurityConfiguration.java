2372e9e85918d0cd4f4d3361d0bf132c38e5a917
testbest <tunawillgetit1030@gmail.com>
2026-03-11 00:14:10 +0700

    update auth controller, permisison and role

--- backend/src/main/java/com/tuan/jobmarket/config/SecurityConfiguration.java
+++ backend/src/main/java/com/tuan/jobmarket/config/SecurityConfiguration.java
@@ -42,7 +42,8 @@ public class SecurityConfiguration {
 
         String[] whiteList = {
                 "/",
-                "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**",
+                "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
+                "/storage/**",
                 "/api/v1/companies/**", "/api/v1/jobs/**"
         };
         http
