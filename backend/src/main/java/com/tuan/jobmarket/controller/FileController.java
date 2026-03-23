23e49a2b76764adedc6f6b44a29dee9b2f591dd9
testbest <tunawillgetit1030@gmail.com>
2026-03-23 16:40:38 +0700

    fix file controller

--- backend/src/main/java/com/tuan/jobmarket/controller/FileController.java
+++ backend/src/main/java/com/tuan/jobmarket/controller/FileController.java
@@ -1,20 +1,9 @@
 package com.tuan.jobmarket.controller;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,69 +19,10 @@ import com.tuan.jobmarket.util.error.StorageException;
 @RequestMapping("/api/v1")
 public class FileController {
 
-    @Value("${tuanjobmarket.upload-file.base-path}")
-    private String baseURI;
-    
-    @Autowired
     private final FileService fileService;
 
     public FileController(FileService fileService) {
         this.fileService = fileService;
-    }
-
-    // @PostMapping("/files")
-    // @ApiMessage("Upload single file")
-    // public ResponseEntity<ResUploadFileDTO> upload(
-    //         @RequestParam(name = "file", required = false) MultipartFile file,
-    //         @RequestParam("folder") String folder
-
-    // ) throws URISyntaxException, IOException, StorageException {
-    //     // skip validate
-    //     if (file == null || file.isEmpty()) {
-    //         throw new StorageException("File is empty. Please upload a file.");
-    //     }
-    //     String fileName = file.getOriginalFilename();
-    //     List<String> allowedExtensions = Arrays.asList("pdf", "jpg", "jpeg", "png", "doc", "docx");
-    //     boolean isValid = allowedExtensions.stream().anyMatch(item -> fileName.toLowerCase().endsWith(item));
-
-    //     if (!isValid) {
-    //         throw new StorageException("Invalid file extension. only allows " + allowedExtensions.toString());
-    //     }
-    //     // create a directory if not exist
-    //     this.fileService.createDirectory(baseURI + folder);
-
-    //     // store file
-    //     String uploadFile = this.fileService.store(file, folder);
-
-    //     ResUploadFileDTO res = new ResUploadFileDTO(uploadFile, Instant.now());
-
-    //     return ResponseEntity.ok().body(res);
-    // }
-
-    @GetMapping("/files")
-    @ApiMessage("Download a file")
-    public ResponseEntity<Resource> download(
-            @RequestParam(name = "fileName", required = false) String fileName,
-            @RequestParam(name = "folder", required = false) String folder)
-            throws StorageException, URISyntaxException, FileNotFoundException {
-        if (fileName == null || folder == null) {
-            throw new StorageException("Missing required params : (fileName or folder) in query params.");
-        }
-
-        // check file exist (and not a directory)
-        long fileLength = this.fileService.getFileLength(fileName, folder);
-        if (fileLength == 0) {
-            throw new StorageException("File with name = " + fileName + " not found.");
-        }
-
-        // download a file
-        InputStreamResource resource = this.fileService.getResource(fileName, folder);
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
-                .contentLength(fileLength)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
     }
 
     @PostMapping("/files")
@@ -102,7 +32,6 @@ public class FileController {
             @RequestParam(name = "folder", defaultValue = "general") String folder
     ) throws IOException, StorageException {
 
-        // store() bên trong đã validate file và extension
         String fileUrl = this.fileService.store(file, folder);
 
         ResUploadFileDTO res = new ResUploadFileDTO(fileUrl, Instant.now());
